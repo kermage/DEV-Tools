962458ed9c8bd6d022d1d487ddbedddad95212ef
kermage <genealyson.torcende@gmail.com>
2015-11-09 11:26:34 +0800

    corrected title and call git_config

--- setup.bat
+++ setup.bat
@@ -1,8 +1,11 @@
 @ECHO OFF &MODE CON: COLS=45 LINES=2
-TITLE Setup DevTools
+TITLE Setup DEV-Tools
 
 CALL "import_cmder.bat"
 CALL "import_sublime.bat"
+CALL "git_config.bat"
+
+:: Register Cmder context menu
 %SYSTEMDRIVE%\Cmder\Cmder /REGISTER ALL
 
 PAUSE
