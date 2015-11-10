3c29199f538811f6f470dc0b1205762559ef7efe
kermage <genealyson.torcende@gmail.com>
2015-11-10 17:05:05 +0800

    check existing installer before download

--- setup.bat
+++ setup.bat
@@ -2,13 +2,19 @@
 TITLE Setup DEV-Tools
 
 :: Download tools
-ECHO Downloading Cmder . . .
-cscript "download.vbs" "http://github.com/cmderdev/cmder/releases/download/v1.2.9/cmder_mini.zip" 2>NUL >NUL
-ECHO Downloading Sublime . . .
-cscript "download.vbs" "http://c758482.r82.cf2.rackcdn.com/Sublime Text Build 3083 Setup.exe" 2>NUL >NUL
-ECHO Downloading Git . . .
-cscript "download.vbs" "https://github.com/git-for-windows/git/releases/download/v2.6.2.windows.1/Git-2.6.2-32-bit.exe" 2>NUL >NUL
-
+IF NOT EXIST "cmder_mini.zip" (
+	ECHO Downloading Cmder . . .
+	cscript "download.vbs" "http://github.com/cmderdev/cmder/releases/download/v1.2.9/cmder_mini.zip" 2>NUL >NUL
+)
+IF NOT EXIST "Sublime Text Build 3083 Setup.exe" (
+	ECHO Downloading Sublime . . .
+	cscript "download.vbs" "http://c758482.r82.cf2.rackcdn.com/Sublime Text Build 3083 Setup.exe" 2>NUL >NUL
+)
+IF NOT EXIST "Git-2.6.2-32-bit.exe" (
+	ECHO Downloading Git . . .
+	cscript "download.vbs" "https://github.com/git-for-windows/git/releases/download/v2.6.2.windows.1/Git-2.6.2-32-bit.exe" 2>NUL >NUL
+)
+PAUSE
 :: Install then import config and data
 IF EXIST "cmder_mini.zip" (
 	ECHO Installing Cmder . . .
