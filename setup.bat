50299fb160b78eba1e704c009a4d104a9681e4ae
kermage <genealyson.torcende@gmail.com>
2015-11-10 16:56:13 +0800

    check download before install

--- setup.bat
+++ setup.bat
@@ -10,18 +10,23 @@ ECHO Downloading Git . . .
 cscript "download.vbs" "https://github.com/git-for-windows/git/releases/download/v2.6.2.windows.1/Git-2.6.2-32-bit.exe" 2>NUL >NUL
 
 :: Install then import config and data
-ECHO Installing Cmder . . .
-7za x "cmder_mini.zip" -o"%SYSTEMDRIVE%\Cmder" -y 2>NUL >NUL
-CALL "import_cmder.bat" 2>NUL >NUL
-ECHO Installing Sublime . . .
-"Sublime Text Build 3083 Setup.exe" /SILENT
-CALL "import_sublime.bat" 2>NUL >NUL
-ECHO Installing Git . . .
-"Git-2.6.2-32-bit.exe" /SILENT
-CALL "git_config.bat"
-
-:: Register Cmder context menu
-%SYSTEMDRIVE%\Cmder\Cmder /REGISTER ALL
+IF EXIST "cmder_mini.zip" (
+	ECHO Installing Cmder . . .
+	7za x "cmder_mini.zip" -o"%SYSTEMDRIVE%\Cmder" -y 2>NUL >NUL
+	:: Register Cmder context menu
+	%SYSTEMDRIVE%\Cmder\Cmder /REGISTER ALL
+	CALL "import_cmder.bat" 2>NUL >NUL
+)
+IF EXIST "Sublime Text Build 3083 Setup.exe" (
+	ECHO Installing Sublime . . .
+	"Sublime Text Build 3083 Setup.exe" /SILENT
+	CALL "import_sublime.bat" 2>NUL >NUL
+)
+IF EXIST "Git-2.6.2-32-bit.exe" (
+	ECHO Installing Git . . .
+	"Git-2.6.2-32-bit.exe" /SILENT
+	CALL "git_config.bat"
+)
 
 :: Create ssh key folder if not exist
 IF NOT EXIST "%USERPROFILE%\.ssh\" (
