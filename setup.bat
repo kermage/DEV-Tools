d19a930c14043993fd2b42e17daa619900a8b862
kermage <genealyson.torcende@gmail.com>
2015-11-12 03:09:34 +0800

    import first

--- setup.bat
+++ setup.bat
@@ -34,17 +34,17 @@ IF NOT EXIST "Downloads\Git-2.6.2-32-bit.exe" (
 IF EXIST "Downloads\cmder_mini.zip" (
 	ECHO|SET /P ="Installing Cmder ... "
 	7za x "Downloads\cmder_mini.zip" -o"%SYSTEMDRIVE%\Cmder" -y 2>NUL >NUL
+	CALL "import_cmder.bat" 2>NUL >NUL
 	:: Register Cmder context menu
 	%SYSTEMDRIVE%\Cmder\Cmder /REGISTER ALL
-	CALL "import_cmder.bat" 2>NUL >NUL
 	ECHO DONE^!
 )
 IF EXIST "Downloads\Sublime Text Build 3083 Setup.exe" (
 	ECHO|SET /P ="Installing Sublime ... "
 	"Downloads\Sublime Text Build 3083 Setup.exe" /SILENT /TASKS="contextentry"
+	CALL "import_sublime.bat" 2>NUL >NUL
 	:: Install Package Control
 	cscript "download.vbs" "https://packagecontrol.io/Package Control.sublime-package" "%APPDATA%\Sublime Text 3\Installed Packages" 2>NUL >NUL
-	CALL "import_sublime.bat" 2>NUL >NUL
 	ECHO DONE^!
 )
 IF EXIST "Downloads\Git-2.6.2-32-bit.exe" (
