fa5cc3bd41aa17a478e4eae81e1a664f608c8a56
kermage <genealyson.torcende@gmail.com>
2015-11-14 22:17:33 +0800

    only install missing packages

--- npm_install.bat
+++ npm_install.bat
@@ -18,7 +18,20 @@ SET "PATH=%nodejs_path%;%PATH%"
 :: NPM packages to install
 SET /P npm_packages=Enter Packages: %=%
 
-:: Install npm packages
-(npm install --global %npm_packages%) 2>NUL >NUL
+:: Get NPM global root folder
+FOR /F "tokens=*" %%I IN ('ls ^| npm root -g') DO SET npm_root=%%I
+
+:: Check if npm package are installed
+FOR %%F IN (%npm_packages%) DO CALL :CHECK_PACKAGE %%F
+
+:: Install missing npm packages
+IF NOT "%package_list%" == "" (npm install --global %package_list%) 2>NUL >NUL
+GOTO:EOF
+
+:CHECK_PACKAGE
+:: Add to list if npm package is not installed
+FOR /F "tokens=*" %%J IN ('ls %npm_root% ^| grep "%1"') DO SET is_installed=%%J
+IF "%is_installed%" == "" SET package_list=%1 %package_list%
+GOTO:EOF
 
 :END
