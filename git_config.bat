3649d722072b8bb061f71748e5f08bf89ee351a4
kermage <genealyson.torcende@gmail.com>
2015-11-15 00:53:36 +0800

    set global git config; defaults to current set value

--- git_config.bat
+++ git_config.bat
@@ -21,29 +21,37 @@ ECHO "%PATH%" | FINDSTR /C:"%git_path%" 2>NUL >NUL || (
 )
 
 :MAIN
-:: Check global git config; set if not found
+:: Set global git config
 FOR /F "delims=" %%* IN ('git config user.name') DO SET user_name=%%*
 IF [!user_name!]==[] (
 	SET /P user_name=Enter Username: %=%
-	git config --global user.name !user_name!
+) ELSE (
+	SET /P user_name=Enter Username ^(%user_name%^): %=%
 )
+git config --global user.name %user_name%
 
 FOR /F "delims=" %%* IN ('git config user.email') DO SET user_email=%%*
 IF [!user_email!]==[] (
 	SET /P user_email=Enter Email: %=%
-	git config --global user.email !user_email!
+) ELSE (
+	SET /P user_email=Enter Email ^(%user_email%^): %=%
 )
+git config --global user.email %user_email%
 
 FOR /F "delims=" %%* IN ('git config github.token') DO SET github_token=%%*
 IF [!github_token!]==[] (
 	SET /P github_token=GitHub Token: %=%
-	git config --global github.token !github_token!
+) ELSE (
+	SET /P github_token=GitHub Token ^(%github_token%^): %=%
 )
+git config --global github.token %github_token%
 
 FOR /F "delims=" %%* IN ('git config bitbucket.token') DO SET bitbucket_token=%%*
 IF [!bitbucket_token!]==[] (
 	SET /P bitbucket_token=Bitbucket Token: %=%
-	git config --global bitbucket.token !bitbucket_token!
+) ELSE (
+	SET /P bitbucket_token=Bitbucket Token ^(%bitbucket_token%^): %=%
 )
+git config --global bitbucket.token %bitbucket_token%
 
 :END
