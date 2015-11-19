788d4201cb04e1785b604f7268302f0b27267620
kermage <genealyson.torcende@gmail.com>
2015-11-20 00:06:09 +0800

    add sublime permanently to local env var PATH

--- Cmder-config/bin/add-sublime.bat
+++ Cmder-config/bin/add-sublime.bat
@@ -13,9 +13,18 @@ IF EXIST "%ProgramFiles%\Sublime Text 3" (
 :: Skip if Sublime not found
 IF [!sublime_path!]==[] GOTO :END
 
+:: Check environment variable PATH
+FOR /F "tokens=2*" %%A IN ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PATH') DO SET system_path=%%B
+FOR /F "tokens=2*" %%A IN ('reg query "HKCU\Environment" /v PATH') DO SET user_path=%%B
+
 :: Add Sublime to path if not exist
 ECHO "%PATH%" | FINDSTR /C:"%sublime_path%" 2>NUL >NUL || (
 	SET "PATH=%sublime_path%;%PATH%"
+	ECHO "%system_path%" | FINDSTR /C:"%sublime_path%" 2>NUL >NUL || (
+		ECHO "%user_path%" | FINDSTR /C:"%sublime_path%" 2>NUL >NUL || (
+			SETX PATH "%user_path%;%sublime_path%"
+		)
+	)
 )
 
 :END
