35c8236a30b159fb3e09583b418f221f0ef5bef6
kermage <genealyson.torcende@gmail.com>
2016-06-05 05:18:51 +0800

    only set user folders alias once

--- Cmder-config/bin/add-folders.bat
+++ Cmder-config/bin/add-folders.bat
@@ -1,8 +1,24 @@
 @ECHO OFF
 
-:: Find and set current user shell folders
-FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Documents"') DO CALL alias dc=cd /d "%%G"
-FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Downloads"') DO CALL alias dl=cd /d "%%G"
-FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Desktop"') DO CALL alias dt=cd /d "%%G"
+:: Check if documents alias already set
+FOR /F "tokens=3 delims= " %%A IN ('TYPE %ALIASES% ^| FINDSTR /C:"dc="') DO SET documents_path=%%A
+IF "%documents_path%"=="" (
+	:: Find and set current user documents
+	FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Documents"') DO CALL alias dc=cd /d "%%G"
+)
+
+:: Check if downloads alias already set
+FOR /F "tokens=3 delims= " %%A IN ('TYPE %ALIASES% ^| FINDSTR /C:"dl="') DO SET downloads_path=%%A
+IF "%downloads_path%"=="" (
+	:: Find and set current user downloads
+	FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Downloads"') DO CALL alias dl=cd /d "%%G"
+)
+
+:: Check if desktop alias already set
+FOR /F "tokens=3 delims= " %%A IN ('TYPE %ALIASES% ^| FINDSTR /C:"dt="') DO SET desktop_path=%%A
+IF "%desktop_path%"=="" (
+	:: Find and set current user desktop
+	FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Desktop"') DO CALL alias dt=cd /d "%%G"
+)
 
 :END
