cbe037c8feefed293915e3c994399efd6f244a8f
kermage <genealyson.torcende@gmail.com>
2016-06-05 04:22:30 +0800

    wrap with quotes for path with space

--- Cmder-config/bin/add-folders.bat
+++ Cmder-config/bin/add-folders.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 :: Find and set current user shell folders
-FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Documents"') DO CALL alias dc=cd /d %%G
-FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Downloads"') DO CALL alias dl=cd /d %%G
-FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Desktop"') DO CALL alias dt=cd /d %%G
+FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Documents"') DO CALL alias dc=cd /d "%%G"
+FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Downloads"') DO CALL alias dl=cd /d "%%G"
+FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Desktop"') DO CALL alias dt=cd /d "%%G"
 
 :END
