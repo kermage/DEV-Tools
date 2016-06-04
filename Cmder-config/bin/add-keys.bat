10a23d3fdbb47bb3e464d3be505cb50f42e97b82
kermage <genealyson.torcende@gmail.com>
2016-06-04 14:53:02 +0800

    add keys individually

--- Cmder-config/bin/add-keys.bat
+++ Cmder-config/bin/add-keys.bat
@@ -6,15 +6,13 @@ DIR /B "%USERPROFILE%\.ssh\*_rsa" 2>NUL >NUL || GOTO :END
 :: Check if ssh keys are known
 FOR /F "tokens=*" %%I IN ('DIR /B "%USERPROFILE%\.ssh\*_rsa"') DO CALL :CHECK_KEY %%I
 
-:: Add missing ssh keys
-IF NOT "%ssh_keys%" == "" ssh-add %ssh_keys%
 GOTO :END
 
 :CHECK_KEY
 :: Add to list if ssh key is unidentified
 SET is_identified=
 FOR /F "tokens=*" %%J IN ('ssh-add -l ^| FINDSTR /C:"%1"') DO SET is_identified=%%J
-IF "%is_identified%" == "" SET ssh_keys='%USERPROFILE%\.ssh\%1' %ssh_keys%
+IF "%is_identified%" == "" ssh-add "%USERPROFILE%\.ssh\%1"
 GOTO:EOF
 
 :END
