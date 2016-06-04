1db7d7ae249bce85472d1f380beeae4aa1532a54
kermage <genealyson.torcende@gmail.com>
2016-06-05 03:37:47 +0800

    skip unnecessary set and checks

--- Cmder-config/bin/add-folders.bat
+++ Cmder-config/bin/add-folders.bat
@@ -1,11 +1,8 @@
 @ECHO OFF
 
 :: Find and set current user shell folders
-FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Documents"') DO SET documents_path=%%G
-IF NOT "%documents_path%"=="" CALL alias dc=cd /d "%documents_path%"
-FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Downloads"') DO SET downloads_path=%%G
-IF NOT "%downloads_path%"=="" CALL alias dl=cd /d "%downloads_path%"
-FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Desktop"') DO SET desktop_path=%%G
-IF NOT "%desktop_path%"=="" CALL alias dt=cd /d "%desktop_path%"
+FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Documents"') DO CALL alias dc=cd /d %%G
+FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Downloads"') DO CALL alias dl=cd /d %%G
+FOR /F "tokens=3 delims= " %%G IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /s ^| FINDSTR /C:"Desktop"') DO CALL alias dt=cd /d %%G
 
 :END
