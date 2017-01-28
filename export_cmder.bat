594baa1de91eccca82da3f326aab6d1d238bb2be
kermage <genealyson.torcende@gmail.com>
2017-01-28 08:26:17 +0800

    update cmder setup scripts

--- export_cmder.bat
+++ export_cmder.bat
@@ -1,12 +1,10 @@
 @ECHO OFF
-SET CMDER=Cmder
 
 :COPYFILES
 :: Check if folder exist
-IF NOT EXIST "%SYSTEMDRIVE%\%CMDER%\" GOTO :END
-COPY "%SYSTEMDRIVE%\%CMDER%\vendor\conemu-maximus5\ConEmu.xml" "%SYSTEMDRIVE%\%CMDER%\config\ConEmu.xml"
-ROBOCOPY "%SYSTEMDRIVE%\%CMDER%\bin" "%CMDER%-config\bin" /S /XF "alias.bat" "Readme.md"
-ROBOCOPY "%SYSTEMDRIVE%\%CMDER%\config" "%CMDER%-config\config" /S /XF ".history" "cmder.lua" "Readme.md"
+IF NOT EXIST "%SYSTEMDRIVE%\cmder\" GOTO :END
+ROBOCOPY "%SYSTEMDRIVE%\cmder\bin" "cmder-config\bin" /S /XF "alias.bat" "Readme.md"
+ROBOCOPY "%SYSTEMDRIVE%\cmder\config" "cmder-config\config" /S /XF ".history" "ConEmu.xml" "Readme.md"
 GOTO:EOF
 
 :END
--- import_cmder.bat
+++ import_cmder.bat
@@ -1,11 +1,9 @@
 @ECHO OFF
-SET CMDER=Cmder
 
 :COPYFILES
 :: Check if folder exist
-IF NOT EXIST "%CMDER%-config\" GOTO :END
-ROBOCOPY "%CMDER%-config" "%SYSTEMDRIVE%\%CMDER%" /S
-COPY "%SYSTEMDRIVE%\%CMDER%\config\ConEmu.xml" "%SYSTEMDRIVE%\%CMDER%\vendor\conemu-maximus5\ConEmu.xml"
+IF NOT EXIST "cmder-config\" GOTO :END
+ROBOCOPY "cmder-config" "%SYSTEMDRIVE%\cmder" /S
 GOTO:EOF
 
 :END
