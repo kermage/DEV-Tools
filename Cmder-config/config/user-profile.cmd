7c1e6febad99e3120146d9e18428765b370e8111
kermage <genealyson.torcende@gmail.com>
2017-01-28 11:31:50 +0800

    resaved with correct line endings; editorconfig

--- Cmder-config/config/user-profile.cmd
+++ Cmder-config/config/user-profile.cmd
@@ -1,5 +1,5 @@
 :: use this file to run your own startup commands
 :: use @ in front of the command to prevent printing the command
- 
+
 @CALL "%GIT_INSTALL_ROOT%\cmd\start-ssh-agent.cmd"
 @CALL "%CMDER_ROOT%\bin\add-keys.bat"
