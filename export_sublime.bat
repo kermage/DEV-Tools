ee707bec3bb1e3b1831d10a7ac3b395d3b59de3f
kermage <genealyson.torcende@gmail.com>
2017-01-28 09:32:13 +0800

    update sublime setup scripts

--- export_sublime.bat
+++ export_sublime.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
-SET SUBLIME=Sublime Text 3
 
 :COPYFILES
 :: Check if folder exist
-IF NOT EXIST "%APPDATA%\%SUBLIME%\" GOTO :END
-ROBOCOPY "%APPDATA%\%SUBLIME%\Packages\User" "%SUBLIME%-data\Packages\User" /S /XF "Package Control.last-run" /XD "Color Highlighter" "Package Control.cache"
+IF NOT EXIST "%APPDATA%\Sublime Text 3\Packages\User" GOTO :END
+ROBOCOPY "%APPDATA%\Sublime Text 3\Packages\User" "Sublime Text 3-data" *.sublime-settings *.sublime-keymap
+ROBOCOPY "%APPDATA%\Sublime Text 3\Packages\User\Snippets" "Sublime Text 3-data\Snippets" *.sublime-snippet
 GOTO:EOF
 
 :END
