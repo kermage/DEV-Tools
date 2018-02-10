43e91fe71c4f4864c4ce266bd192d8a58defa150
kermage <genealyson.torcende@gmail.com>
2018-02-10 17:37:21 +0800

    use latest release

--- config.bat
+++ config.bat
@@ -1,8 +1,8 @@
 :: URL
 SET cmder_url=https://github.com/cmderdev/cmder/releases/download/v1.3.4/cmder_mini.zip
 SET sublime_url=https://download.sublimetext.com/Sublime Text Build 3143 Setup.exe
-SET git_url=https://github.com/git-for-windows/git/releases/download/v2.15.0.windows.1/Git-2.15.0-32-bit.exe
-SET node_url=https://nodejs.org/dist/v9.2.0/node-v9.2.0-x86.msi
+SET git_url=https://github.com/git-for-windows/git/releases/download/v2.16.1.windows.4/Git-2.16.1.4-32-bit.exe
+SET node_url=https://nodejs.org/dist/v9.5.0/node-v9.5.0-x86.msi
 
 :: Filenames
 FOR %%F IN ( "%cmder_url%" ) DO SET cmder_fn=%%~nxF
