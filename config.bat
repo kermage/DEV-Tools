ec442da09f36538cde8b1f7d30757b633315340f
kermage <genealyson.torcende@gmail.com>
2018-11-01 08:13:18 +0800

    use latest release

--- config.bat
+++ config.bat
@@ -1,6 +1,6 @@
 :: URL
-SET cmder_url=https://github.com/cmderdev/cmder/releases/download/v1.3.4/cmder_mini.zip
-SET sublime_url=https://download.sublimetext.com/Sublime Text Build 3143 Setup.exe
+SET cmder_url=https://github.com/cmderdev/cmder/releases/download/v1.3.6/cmder_mini.zip
+SET sublime_url=https://download.sublimetext.com/Sublime Text Build 3176 Setup.exe
 
 :: Filenames
 FOR %%F IN ( "%cmder_url%" ) DO SET cmder_fn=%%~nxF
