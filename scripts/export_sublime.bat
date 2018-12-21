ece4a1902c9a4e16c6effa909db775c3c424223b
kermage <genealyson.torcende@gmail.com>
2018-12-21 18:36:46 +0800

    st3 settings in different folder

--- scripts/export_sublime.bat
+++ scripts/export_sublime.bat
@@ -3,7 +3,7 @@
 :COPYFILES
 :: Check if folder exist
 IF NOT EXIST "%APPDATA%\Sublime Text 3\Packages\User" GOTO :END
-ROBOCOPY "%APPDATA%\Sublime Text 3\Packages\User" "..\Sublime Text 3-data" *.sublime-settings *.sublime-keymap
+ROBOCOPY "%APPDATA%\Sublime Text 3\Packages\User" "..\Sublime Text 3-data\Settings" *.sublime-settings *.sublime-keymap
 ROBOCOPY "%APPDATA%\Sublime Text 3\Packages\User\Snippets" "..\Sublime Text 3-data\Snippets" *.sublime-snippet
 GOTO:EOF
 
