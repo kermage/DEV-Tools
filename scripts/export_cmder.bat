847eb92dd8fc97f6d18f1ce7c9c0e37d2f77c351
kermage <genealyson.torcende@gmail.com>
2018-12-23 01:18:33 +0800

    update Cmder import and export script

--- scripts/export_cmder.bat
+++ scripts/export_cmder.bat
@@ -3,8 +3,7 @@
 :COPYFILES
 :: Check if folder exist
 IF NOT EXIST "%SYSTEMDRIVE%\Cmder\" GOTO :END
-ROBOCOPY "%SYSTEMDRIVE%\Cmder\bin" "..\Cmder-config\bin" /S /XF "alias.bat" "Readme.md"
-ROBOCOPY "%SYSTEMDRIVE%\Cmder\config" "..\Cmder-config\config" /S /XF ".history" "ConEmu.xml" "Readme.md"
+ROBOCOPY "%SYSTEMDRIVE%\Cmder\config" "..\Cmder-config" /S /XF ".history" "ConEmu.xml" "Readme.md"
 GOTO:EOF
 
 :END
--- scripts/import_cmder.bat
+++ scripts/import_cmder.bat
@@ -3,7 +3,7 @@
 :COPYFILES
 :: Check if folder exist
 IF NOT EXIST "Cmder-config\" GOTO :END
-ROBOCOPY "Cmder-config" "%SYSTEMDRIVE%\Cmder" /S
+ROBOCOPY "Cmder-config" "%SYSTEMDRIVE%\Cmder\config" /S
 GOTO:EOF
 
 :END
