eb3b6afbd5953291c896ed2b32ae397913b3db5d
kermage <genealyson.torcende@gmail.com>
2018-12-24 00:58:55 +0800

    remove from old ConEmu

--- scripts/export_cmder.bat
+++ scripts/export_cmder.bat
@@ -3,7 +3,7 @@
 :COPYFILES
 :: Check if folder exist
 IF NOT EXIST "%SYSTEMDRIVE%\Cmder\" GOTO :END
-ROBOCOPY "%SYSTEMDRIVE%\Cmder\config" "..\Cmder-config" /S /XF ".history" "ConEmu.xml" "Readme.md"
+ROBOCOPY "%SYSTEMDRIVE%\Cmder\config" "..\Cmder-config" /S /XF ".history" "Readme.md"
 GOTO:EOF
 
 :END
