e4787e781d1f1e5b7f965c9b217381ff03b927e6
kermage <genealyson.torcende@gmail.com>
2019-04-18 00:57:40 +0800

    use latest cmder and sublime

--- config.bat
+++ config.bat
@@ -1,6 +1,6 @@
 :: URL
-SET cmder_url=https://github.com/cmderdev/cmder/releases/download/v1.3.10/cmder_mini.zip
-SET sublime_url=https://download.sublimetext.com/Sublime Text Build 3176 Setup.exe
+SET cmder_url=https://github.com/cmderdev/cmder/releases/download/v1.3.11/cmder_mini.zip
+SET sublime_url=https://download.sublimetext.com/Sublime Text Build 3207 Setup.exe
 
 :: Filenames
 FOR %%F IN ( "%cmder_url%" ) DO SET cmder_fn=%%~nxF
