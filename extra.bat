abee3d2fd14e8d9ecf1e340baf1d976ef542d8f1
kermage <genealyson.torcende@gmail.com>
2020-05-07 19:12:55 +0800

    add our fancy banner

--- extra.bat
+++ extra.bat
@@ -1,6 +1,22 @@
-
 @ECHO OFF
 SETLOCAL EnableDelayedExpansion
+TITLE Extra DEV-Tools
+
+:MAIN
+:: Change to batch file's directory
+CD /D %~dp0
+
+:: Banner
+ECHO.
+ECHO    *******    ******** **      **     **********                   **
+ECHO   /**////**  /**///// /**     /**    /////**///                   /**
+ECHO   /**    /** /**      /**     /**        /**    ******    ******  /**   ******
+ECHO   /**    /** /******* //**    **  *****  /**   **////**  **////** /**  **////
+ECHO   /**    /** /**////   //**  **  /////   /**  /**   /** /**   /** /** //*****
+ECHO   /**    **  /**        //****           /**  /**   /** /**   /** /**  /////**
+ECHO   /*******   /********   //**            /**  //******  //******  ***  ******
+ECHO   ///////    ////////     //             //    //////    //////  ///  //////
+ECHO.
 
 SET sources[0]=https://cdn1.evernote.com/win6/public/Evernote_6.24.2.8919.exe
 SET sources[1]=https://cdn.localwp.com/releases-stable/5.4.1+3311/local-5.4.1-windows.exe
@@ -14,6 +30,7 @@ FOR %%F IN ( "%source%" ) DO SET file_name=%%~nxF
 
 CALL :DOWNLOAD "%file_name%" "%source%"
 CALL :INSTALL "%file_name%"
+ECHO.
 
 SET /A index+=1
 GOTO :LOOP
