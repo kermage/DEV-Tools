49edd3d0c6bf228acbbf811f8234585878ca4177
kermage <genealyson.torcende@gmail.com>
2020-05-07 23:59:52 +0800

    cleanup files

--- scripts/export_cmder.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-:COPYFILES
-:: Check if folder exist
-IF NOT EXIST "%SCOOP%\persist\cmder\" GOTO :END
-ROBOCOPY "%SCOOP%\persist\cmder\config" "..\Cmder-config" /S /XF ".history" "Readme.md"
-
-:END
-PAUSE
