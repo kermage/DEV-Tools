676aa25c5fb7836ae3e35f1d41e52495644e52e5
kermage <genealyson.torcende@gmail.com>
2020-05-07 23:18:33 +0800

    include nodejs, php, and composer

--- scripts/export_scoop.bat
+++ scripts/export_scoop.bat
@@ -4,6 +4,8 @@
 :: Check if folder exist
 IF NOT EXIST "%SCOOP%\persist\" GOTO :END
 ROBOCOPY "%SCOOP%\persist\cmder\config" "..\scoop-data\cmder" /S /XF ".history" "Readme.md"
+ROBOCOPY "%SCOOP%\persist\composer\home" "..\scoop-data\composer" *.json keys.*
+ROBOCOPY "%SCOOP%\persist\php\cli" "..\scoop-data\php" /S
 ROBOCOPY "%SCOOP%\persist\sublime-text\Data\Packages\User" "..\scoop-data\sublime-text\settings" *.sublime-settings *.sublime-keymap
 ROBOCOPY "%SCOOP%\persist\sublime-text\Data\Packages\User\Snippets" "..\scoop-data\sublime-text\snippets" *.sublime-snippet
 
--- scripts/import_scoop.bat
+++ scripts/import_scoop.bat
@@ -5,6 +5,8 @@
 IF NOT EXIST "scoop-data\" GOTO :END
 ROBOCOPY "scoop-data\cmder" "%SCOOP%\persist\cmder\config" /S /NS /NC /NJH /NJS
 COPY "scoop-data\cmder\user-ConEmu.xml" "%SCOOP%\persist\cmder\vendor\conemu-maximus5\ConEmu.xml"
+ROBOCOPY "scoop-data\composer" "%SCOOP%\persist\composer\home" composer.* /S /NS /NC /NJH /NJS
+ROBOCOPY "scoop-data\php" "%SCOOP%\persist\php\cli" /S /NS /NC /NJH /NJS
 ROBOCOPY "scoop-data\sublime-text\settings" "%SCOOP%\persist\sublime-text\Data\Packages\User" /S /NS /NC /NJH /NJS
 ROBOCOPY "scoop-data\sublime-text\snippets" "%SCOOP%\persist\sublime-text\Data\Packages\User\Snippets" /S /NS /NC /NJH /NJS
 GOTO:EOF
