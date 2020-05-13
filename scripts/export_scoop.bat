ec1609012a75757d4049ad6842fe3f882c55a06d
kermage <genealyson.torcende@gmail.com>
2020-05-13 08:34:07 +0800

    follow the local history path

--- scripts/export_scoop.bat
+++ scripts/export_scoop.bat
@@ -10,6 +10,7 @@ ROBOCOPY "%SCOOP%\persist\php\cli" "..\scoop-data\php" /S
 POWERSHELL -Command "( gc ..\scoop-data\php\php.ini ) -replace 'curl.cainfo=.+', ';curl.cainfo=' | Out-File -encoding UTF8 ..\scoop-data\php\php.ini"
 POWERSHELL -Command "( gc ..\scoop-data\php\php.ini ) -replace 'openssl.cafile=.+', ';openssl.cafile=' | Out-File -encoding UTF8 ..\scoop-data\php\php.ini"
 ROBOCOPY "%SCOOP%\persist\sublime-text\Data\Packages\User" "..\scoop-data\sublime-text\settings" *.sublime-settings *.sublime-keymap
+POWERSHELL -Command "( gc ..\scoop-data\sublime-text\settings\LocalHistory.sublime-settings ) -replace ': "".+/Sublime Text Backups', ': """"###DEV_TOOLS###/Sublime Text Backups' | Out-File -encoding UTF8 ..\scoop-data\sublime-text\settings\LocalHistory.sublime-settings"
 ROBOCOPY "%SCOOP%\persist\sublime-text\Data\Packages\User\Snippets" "..\scoop-data\sublime-text\snippets" *.sublime-snippet
 
 :END
--- scripts/import_scoop.bat
+++ scripts/import_scoop.bat
@@ -2,19 +2,20 @@
 
 FOR %%I IN ( "%~dp0\..\.." ) DO SET "DEV_TOOLS=%%~fI"
 CALL SET DEV_TOOLS=%%DEV_TOOLS:\=/%%
-CALL SET DEV_TOOLS=/%%DEV_TOOLS::=%%
+CALL SET DT_UNIX=/%%DEV_TOOLS::=%%
 
 :COPYFILES
 :: Check if folder exist
 IF NOT EXIST "scoop-data\" GOTO :END
 ROBOCOPY "scoop-data\cmder" "%SCOOP%\persist\cmder\config" /S /NS /NC /NJH /NJS
 COPY "scoop-data\cmder\user-ConEmu.xml" "%SCOOP%\persist\cmder\vendor\conemu-maximus5\ConEmu.xml"
-POWERSHELL -Command "( gc %SCOOP%\persist\cmder\config\user_profile.sh ) -replace '###DEV_TOOLS###', '. ""%DEV_TOOLS%' | Out-File -encoding UTF8 %SCOOP%\persist\cmder\config\user_profile.sh"
+POWERSHELL -Command "( gc %SCOOP%\persist\cmder\config\user_profile.sh ) -replace '###DEV_TOOLS###', '. ""%DT_UNIX%' | Out-File -encoding UTF8 %SCOOP%\persist\cmder\config\user_profile.sh"
 ROBOCOPY "scoop-data\composer" "%SCOOP%\persist\composer\home" composer.* /S /NS /NC /NJH /NJS
 ROBOCOPY "scoop-data\php" "%SCOOP%\persist\php\cli" /S /NS /NC /NJH /NJS
 POWERSHELL -Command "( gc %SCOOP%\persist\php\cli\php.ini ) -replace ';curl.cainfo=', 'curl.cainfo=%SCOOP%\persist\php\cli\cacert.pem' | Out-File -encoding UTF8 %SCOOP%\persist\php\cli\php.ini"
 POWERSHELL -Command "( gc %SCOOP%\persist\php\cli\php.ini ) -replace ';openssl.cafile=', 'openssl.cafile=%SCOOP%\persist\php\cli\cacert.pem' | Out-File -encoding UTF8 %SCOOP%\persist\php\cli\php.ini"
 ROBOCOPY "scoop-data\sublime-text\settings" "%SCOOP%\persist\sublime-text\Data\Packages\User" /S /NS /NC /NJH /NJS
+POWERSHELL -Command "( gc %SCOOP%\persist\sublime-text\Data\Packages\User\LocalHistory.sublime-settings ) -replace '###DEV_TOOLS###', '%DEV_TOOLS%' | Out-File -encoding UTF8 %SCOOP%\persist\sublime-text\Data\Packages\User\LocalHistory.sublime-settings"
 ROBOCOPY "scoop-data\sublime-text\snippets" "%SCOOP%\persist\sublime-text\Data\Packages\User\Snippets" /S /NS /NC /NJH /NJS
 GOTO:EOF
 
