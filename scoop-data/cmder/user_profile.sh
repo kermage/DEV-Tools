708795988c297ebb5820355da4f50f91e429b30f
kermage <genealyson.torcende@gmail.com>
2020-05-13 18:51:19 +0800

    normalize line endings

--- scoop-data/cmder/user_profile.sh
+++ scoop-data/cmder/user_profile.sh
@@ -1,4 +1,4 @@
-﻿# use this file to run your own startup commands
+# use this file to run your own startup commands
 
 . "${CMDER_ROOT}/config/scripts/add-keys.sh"
 . "${CMDER_ROOT}/config/user_aliases.sh"
