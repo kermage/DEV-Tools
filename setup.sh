46482018f5dc5bded4b638e8a9db539f0b11df6e
kermage <genealyson.torcende@gmail.com>
2020-05-24 13:58:40 +0800

    automount with metadata

--- setup.sh
+++ setup.sh
@@ -3,3 +3,10 @@ sudo apt-get install zsh zsh-syntax-highlighting zsh-autosuggestions
 chsh -s $(which zsh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
 sudo chmod -R 755 $HOME/.zsh
+
+if [ ! -f /etc/wsl.conf ] ; then
+	cat <<EOT >> /etc/wsl.conf
+[automount]
+options = "metadata"
+EOT
+fi
