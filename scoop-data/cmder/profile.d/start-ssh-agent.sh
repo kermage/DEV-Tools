a801c9355085cc3c633d34c52472fe8aa27c9a72
kermage <genealyson.torcende@gmail.com>
2020-05-24 10:31:09 +0800

    simplify variables

--- scoop-data/cmder/profile.d/start-ssh-agent.sh
+++ scoop-data/cmder/profile.d/start-ssh-agent.sh
@@ -4,10 +4,10 @@ SSH_AGENT_PID=$( ps -f | grep ssh-agent | awk '{print $2}' )
 # Connect up the current ssh-agent
 if [ -z "$SSH_AGENT_PID" ] ; then
 	echo Removing old ssh-agent sockets
-	rm -r ${TEMP}/ssh-* 2>/dev/null
+	rm -r /tmp/ssh-* 2>/dev/null
 else
 	echo Found ssh-agent at $SSH_AGENT_PID
-	SSH_AUTH_SOCK=$( ls ${TEMP}/ssh-*/agent.* | sed 's/=//' )
+	SSH_AUTH_SOCK=$( ls /tmp/ssh-*/agent.* | sed 's/=//' )
 	if [ ! -z "$SSH_AUTH_SOCK" ] ; then
 		echo Found ssh-agent socket at $SSH_AUTH_SOCK
 		export SSH_AGENT_PID
