af4c561ee86a4054ec0893aa4f69845f0d2a3227
kermage <genealyson.torcende@gmail.com>
2020-05-24 08:29:28 +0800

    customize with powerlevel10k

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 sudo apt-get update
 sudo apt-get install zsh zsh-syntax-highlighting zsh-autosuggestions
 chsh -s $(which zsh)
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
+sudo chmod -R 755 $HOME/.zsh
