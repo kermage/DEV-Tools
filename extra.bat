6fd9f3a1aaea6081a11c54c3efeb00d77f884fc9
kermage <genealyson.torcende@gmail.com>
2020-05-28 20:22:38 +0800

    update localwp to 5.5.2

--- extra.bat
+++ extra.bat
@@ -19,7 +19,7 @@ ECHO   ///////    ////////     //             //    //////    //////  ///  /////
 ECHO.
 
 SET sources[0]=https://cdn1.evernote.com/win6/public/Evernote_6.24.2.8919.exe
-SET sources[1]=https://cdn.localwp.com/releases-stable/5.4.1+3311/local-5.4.1-windows.exe
+SET sources[1]=https://cdn.localwp.com/releases-stable/5.5.2+3616/local-5.5.2-windows.exe
 SET index=0
 
 :LOOP
