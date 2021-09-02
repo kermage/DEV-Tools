1e51bb5879e0089a07cab7c804b65e54566e8f87
kermage <genealyson.torcende@gmail.com>
2021-09-02 11:49:03 +0800

    update php data

--- scripts/export_scoop.bat
+++ scripts/export_scoop.bat
@@ -9,9 +9,11 @@ ROBOCOPY "%SCOOP%\persist\composer\home" "..\scoop-data\composer" *.json keys.*
 ROBOCOPY "%SCOOP%\persist\php\cli" "..\scoop-data\php" /S
 POWERSHELL -Command "( gc ..\scoop-data\php\php.ini ) -replace 'curl.cainfo=.+', ';curl.cainfo=' | Out-File -encoding ASCII ..\scoop-data\php\php.ini"
 POWERSHELL -Command "( gc ..\scoop-data\php\php.ini ) -replace 'openssl.cafile=.+', ';openssl.cafile=' | Out-File -encoding ASCII ..\scoop-data\php\php.ini"
+ROBOCOPY "%SCOOP%\persist\php\cli" "..\scoop-data\php74" /S
+POWERSHELL -Command "( gc ..\scoop-data\php74\php.ini ) -replace 'curl.cainfo=.+', ';curl.cainfo=' | Out-File -encoding ASCII ..\scoop-data\php74\php.ini"
+POWERSHELL -Command "( gc ..\scoop-data\php74\php.ini ) -replace 'openssl.cafile=.+', ';openssl.cafile=' | Out-File -encoding ASCII ..\scoop-data\php74\php.ini"
 ROBOCOPY "%SCOOP%\persist\sublime-text\Data\Packages\User" "..\scoop-data\sublime-text\settings" *.sublime-settings *.sublime-keymap
 POWERSHELL -Command "( gc ..\scoop-data\sublime-text\settings\LocalHistory.sublime-settings ) -replace ': "".+/Sublime Text Backups', ': """"###DEV_TOOLS###/Sublime Text Backups' | Out-File -encoding ASCII ..\scoop-data\sublime-text\settings\LocalHistory.sublime-settings"
-POWERSHELL -Command "( gc ..\scoop-data\sublime-text\settings\Terminal.sublime-settings ) -replace ': "".+/apps/cmder/current/Cmder.exe', ': """"###SCOOP###/apps/cmder/current/Cmder.exe' | Out-File -encoding ASCII ..\scoop-data\sublime-text\settings\Terminal.sublime-settings"
 ROBOCOPY "%SCOOP%\persist\sublime-text\Data\Packages\User\Snippets" "..\scoop-data\sublime-text\snippets" *.sublime-snippet
 
 :END
