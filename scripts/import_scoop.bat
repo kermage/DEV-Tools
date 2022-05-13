32b0ff30ae563d958565dc64db1b819a0ae2adef
kermage <genealyson.torcende@gmail.com>
2022-05-13 21:09:42 +0800

    update php with cacert app

--- scripts/import_scoop.bat
+++ scripts/import_scoop.bat
@@ -10,11 +10,11 @@ CALL SET DT_UNIX=/%%DEV_TOOLS::=%%
 IF NOT EXIST "scoop-data\" GOTO :END
 ROBOCOPY "scoop-data\composer" "%SCOOP%\persist\composer\home" composer.* /S /NS /NC /NJH /NJS
 ROBOCOPY "scoop-data\php" "%SCOOP%\persist\php\cli" /S /NS /NC /NJH /NJS
-POWERSHELL -Command "( gc %SCOOP%\persist\php\cli\php.ini ) -replace ';curl.cainfo=', 'curl.cainfo=%SCOOP%\persist\php\cli\cacert.pem' | Out-File -encoding ASCII %SCOOP%\persist\php\cli\php.ini"
-POWERSHELL -Command "( gc %SCOOP%\persist\php\cli\php.ini ) -replace ';openssl.cafile=', 'openssl.cafile=%SCOOP%\persist\php\cli\cacert.pem' | Out-File -encoding ASCII %SCOOP%\persist\php\cli\php.ini"
+POWERSHELL -Command "( gc %SCOOP%\persist\php\cli\php.ini ) -replace ';curl.cainfo=', 'curl.cainfo=%SCOOP%\apps\cacert\current\cacert.pem' | Out-File -encoding ASCII %SCOOP%\persist\php\cli\php.ini"
+POWERSHELL -Command "( gc %SCOOP%\persist\php\cli\php.ini ) -replace ';openssl.cafile=', 'openssl.cafile=%SCOOP%\apps\cacert\current\cacert.pem' | Out-File -encoding ASCII %SCOOP%\persist\php\cli\php.ini"
 ROBOCOPY "scoop-data\php" "%SCOOP%\persist\php74\cli" /S /NS /NC /NJH /NJS
-POWERSHELL -Command "( gc %SCOOP%\persist\php74\cli\php.ini ) -replace ';curl.cainfo=', 'curl.cainfo=%SCOOP%\persist\php74\cli\cacert.pem' | Out-File -encoding ASCII %SCOOP%\persist\php74\cli\php.ini"
-POWERSHELL -Command "( gc %SCOOP%\persist\php74\cli\php.ini ) -replace ';openssl.cafile=', 'openssl.cafile=%SCOOP%\persist\php74\cli\cacert.pem' | Out-File -encoding ASCII %SCOOP%\persist\php74\cli\php.ini"
+POWERSHELL -Command "( gc %SCOOP%\persist\php74\cli\php.ini ) -replace ';curl.cainfo=', 'curl.cainfo=%SCOOP%\apps\cacert\current\cacert.pem' | Out-File -encoding ASCII %SCOOP%\persist\php74\cli\php.ini"
+POWERSHELL -Command "( gc %SCOOP%\persist\php74\cli\php.ini ) -replace ';openssl.cafile=', 'openssl.cafile=%SCOOP%\apps\cacert\current\cacert.pem' | Out-File -encoding ASCII %SCOOP%\persist\php74\cli\php.ini"
 GOTO:EOF
 
 :END
