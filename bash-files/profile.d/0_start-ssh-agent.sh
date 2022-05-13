c83e6420651e0b5c902455e8425ecb28d796fe1d
kermage <genealyson.torcende@gmail.com>
2022-05-13 21:48:47 +0800

    simplified ssh-agent starter

--- bash-files/profile.d/0_start-ssh-agent.sh
+++ bash-files/profile.d/0_start-ssh-agent.sh
@@ -1,36 +1,31 @@
 # Check if the agent is running
-SSH_AGENT_PID=$( ps -f | grep ssh-agent | grep -v grep | awk '{print $2}' )
+SSH_AGENT_PID=$( ps -f | grep ssh-agent | awk '{print $2}' )
+SSH_AUTH_SOCK=$( ls ${TEMP}/ssh-*/agent.* 2>/dev/null | sed 's/=//' )
 
 if [ -z "$SSH_AGENT_PID" ] ; then
-	SSH_AGENT_PID=$( ps -ax 2>/dev/null | grep 'ssh-agent -s' | grep -v grep | awk '{print $1}' )
-fi
-
-# Connect up the current ssh-agent
-if [ -z "$SSH_AGENT_PID" ] ; then
-	echo Removing old ssh-agent sockets
-	rm -r /tmp/ssh-* 2>/dev/null
-else
-	echo Found ssh-agent at $SSH_AGENT_PID
-	SSH_AUTH_SOCK=$( ls /tmp/ssh-*/agent.* | sed 's/=//' )
 	if [ ! -z "$SSH_AUTH_SOCK" ] ; then
-		echo Found ssh-agent socket at $SSH_AUTH_SOCK
+		echo Removing old ssh-agent sockets
+		rm -r ${TEMP}/ssh-* 2>/dev/null
+	fi
+else
+	if [ -z "$SSH_AUTH_SOCK" ] ; then
+		echo Killing old unusable ssh-agents
+		kill $SSH_AGENT_PID
+	else
 		export SSH_AGENT_PID
 		export SSH_AUTH_SOCK
-	else
-		echo Failed to find ssh-agent socket
-		SSH_AGENT_PID=
 	fi
 fi
 
-# See if we have the key
-ssh-add -l > /dev/null 2>&1
-E=$?
-if [ $E -ne 0 ]; then
-	if [ $E -eq 2 ]; then
-		echo -n "Starting ssh-agent: "
-		eval $( ssh-agent -s ) > /dev/null
-		echo "done"
-	fi
-	ssh-add
-	echo
+# See if we have keys
+ssh-add -l &>/dev/null
+if [ $? -eq 2 ]; then
+	# Start a new agent
+	eval $( ssh-agent -s ) > /dev/null
+	echo -n "Agent started with "
+else
+	# Reuse started agent
+	echo -n "Connected to Agent "
 fi
+echo PID: $SSH_AGENT_PID Socket: $SSH_AUTH_SOCK
+echo
