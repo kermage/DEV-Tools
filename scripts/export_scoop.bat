36fd0fa1a5e002e4f888f4a6d1c8094002bf3d9c
kermage <genealyson.torcende@gmail.com>
2022-05-20 08:16:32 +0800

    fix script for php data

--- scripts/export_scoop.bat
+++ scripts/export_scoop.bat
@@ -7,7 +7,7 @@ ROBOCOPY "%SCOOP%\persist\composer\home" "..\scoop-data\composer" *.json keys.*
 ROBOCOPY "%SCOOP%\persist\php\cli" "..\scoop-data\php" /S
 POWERSHELL -Command "( gc ..\scoop-data\php\php.ini ) -replace 'curl.cainfo=.+', ';curl.cainfo=' | Out-File -encoding ASCII ..\scoop-data\php\php.ini"
 POWERSHELL -Command "( gc ..\scoop-data\php\php.ini ) -replace 'openssl.cafile=.+', ';openssl.cafile=' | Out-File -encoding ASCII ..\scoop-data\php\php.ini"
-ROBOCOPY "%SCOOP%\persist\php\cli" "..\scoop-data\php74" /S
+ROBOCOPY "%SCOOP%\persist\php74\cli" "..\scoop-data\php74" /S
 POWERSHELL -Command "( gc ..\scoop-data\php74\php.ini ) -replace 'curl.cainfo=.+', ';curl.cainfo=' | Out-File -encoding ASCII ..\scoop-data\php74\php.ini"
 POWERSHELL -Command "( gc ..\scoop-data\php74\php.ini ) -replace 'openssl.cafile=.+', ';openssl.cafile=' | Out-File -encoding ASCII ..\scoop-data\php74\php.ini"
 
--- scripts/import_scoop.bat
+++ scripts/import_scoop.bat
@@ -1,10 +1,5 @@
 @ECHO OFF
 
-FOR %%I IN ( "%~dp0\..\.." ) DO SET "DEV_TOOLS=%%~fI"
-CALL SET DEV_TOOLS=%%DEV_TOOLS:\=/%%
-CALL SET SCOOP_PATH=%%SCOOP:\=/%%
-CALL SET DT_UNIX=/%%DEV_TOOLS::=%%
-
 :COPYFILES
 :: Check if folder exist
 IF NOT EXIST "scoop-data\" GOTO :END
@@ -12,7 +7,7 @@ ROBOCOPY "scoop-data\composer" "%SCOOP%\persist\composer\home" composer.* /S /NS
 ROBOCOPY "scoop-data\php" "%SCOOP%\persist\php\cli" /S /NS /NC /NJH /NJS
 POWERSHELL -Command "( gc %SCOOP%\persist\php\cli\php.ini ) -replace ';curl.cainfo=', 'curl.cainfo=%SCOOP%\apps\cacert\current\cacert.pem' | Out-File -encoding ASCII %SCOOP%\persist\php\cli\php.ini"
 POWERSHELL -Command "( gc %SCOOP%\persist\php\cli\php.ini ) -replace ';openssl.cafile=', 'openssl.cafile=%SCOOP%\apps\cacert\current\cacert.pem' | Out-File -encoding ASCII %SCOOP%\persist\php\cli\php.ini"
-ROBOCOPY "scoop-data\php" "%SCOOP%\persist\php74\cli" /S /NS /NC /NJH /NJS
+ROBOCOPY "scoop-data\php74" "%SCOOP%\persist\php74\cli" /S /NS /NC /NJH /NJS
 POWERSHELL -Command "( gc %SCOOP%\persist\php74\cli\php.ini ) -replace ';curl.cainfo=', 'curl.cainfo=%SCOOP%\apps\cacert\current\cacert.pem' | Out-File -encoding ASCII %SCOOP%\persist\php74\cli\php.ini"
 POWERSHELL -Command "( gc %SCOOP%\persist\php74\cli\php.ini ) -replace ';openssl.cafile=', 'openssl.cafile=%SCOOP%\apps\cacert\current\cacert.pem' | Out-File -encoding ASCII %SCOOP%\persist\php74\cli\php.ini"
 GOTO:EOF
